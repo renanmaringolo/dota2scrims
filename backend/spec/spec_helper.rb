546c447a5479c2400b6099b803bc504d23e001f7
Renan Maringolo <renan.maringolo@gmail.com>
2026-03-09 01:43:09 -0300

    chore: raise SimpleCov thresholds to 90% global, 80% per file

--- backend/spec/spec_helper.rb
+++ backend/spec/spec_helper.rb
@@ -13,8 +13,8 @@ SimpleCov.start 'rails' do
   add_group 'Jobs', 'app/jobs'
   add_group 'Mailers', 'app/mailers'
 
-  minimum_coverage 80
-  minimum_coverage_by_file 70
+  minimum_coverage 90
+  minimum_coverage_by_file 80
 end
 
 RSpec.configure do |config|
